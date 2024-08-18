4da3fce942c81e857f18dd9ac2e95c9efd07daf3
RiteshRay07 <DeveloperRiteshRay@gmail.com>
2024-08-18 11:18:09 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import crypto from "crypto";
+
+export function generateOTP(length = 6) {
+  if (length < 1) {
+    throw new Error("Length must be a positive integer.");
+  }
+  const digits = "0123456789";
+  let otp = "";
+  const randomBytes = crypto.randomBytes(length);
+  for (let i = 0; i < length; i++) {
+    otp += digits.charAt(randomBytes[i] % digits.length);
+  }
+
+  return otp;
+}
